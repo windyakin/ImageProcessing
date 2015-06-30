2ee492792edafb1ffb4f94ca93a8d86152e8d607
windyakin <windyakin@gmail.com>
2015-06-30 23:57:44 +0900

    グレースケール処理の追加

--- source/script.js
+++ source/script.js
@@ -11,6 +11,7 @@
 			// 変数定義
 			this.canvas = $('#canvas')[0];
 			this.context = canvas.getContext('2d');
+			this.originData = null;
 			this.image = new Image();
 			this.image.onload = $.proxy(this.onloadImage, this);
 			this.reader = new FileReader();
@@ -20,7 +21,11 @@
 			// jQueryイベント
 			$(document)
 				.on('change', '#file', $.proxy(this.changeFile, this))
-				.on('click', '#canvas', $.proxy(this.getColorClick, this));
+				.on('click', '#canvas', $.proxy(this.getColorClick, this))
+				.on('click', '#reset', $.proxy(this.renderOriginalImage, this))
+				.on('click', '#brightness', $.proxy(this.renderGrayScale, this, 'brightness'))
+				.on('click', '#saturation', $.proxy(this.renderGrayScale, this, 'saturation'))
+				.on('click', '#average', $.proxy(this.renderGrayScale, this, 'average'));
 		},
 		// ---------------------------------------------------------------------------------------------
 		// FileReader().onload
@@ -40,6 +45,7 @@
 			this.canvas.height = this.image.height;
 			// 画像を描画
 			this.context.drawImage(this.image, 0, 0);
+			this.originData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
 		},
 		// ---------------------------------------------------------------------------------------------
 		// [jq] 入力ファイルの変更
@@ -49,7 +55,6 @@
 			// ファイルを読み込み
 			this.file = event.target.files[0];
 			console.log(this.file);
-			// canvas上に描画
 			this.reader.readAsDataURL(this.file);
 		},
 		// ---------------------------------------------------------------------------------------------
@@ -60,7 +65,7 @@
 			var y = event.pageY - pos.top;
 			var color = this.getPixelColor(x, y);
 			var luminance = this.getLuminance(color);
-			console.log({color: color, luminance: luminance});
+			console.log(color);
 		},
 		// ---------------------------------------------------------------------------------------------
 		// 画素値の取得
@@ -74,8 +79,72 @@
 		// 輝度の取得
 		// ---------------------------------------------------------------------------------------------
 		getLuminance: function(color) {
+			// NTSC加重平均
 			var luminance = Math.floor(0.298912 * color.r + 0.586611 * color.g + 0.114478 * color.b);
 			return luminance;
+		},
+		// ---------------------------------------------------------------------------------------------
+		// 彩度の取得
+		// ---------------------------------------------------------------------------------------------
+		getSaturation: function(color) {
+			var max = Math.max(color.r, color.g, color.b);
+			var min = Math.min(color.r, color.g, color.b);
+			var saturation = (max + min) / 2;
+			return saturation;
+		},
+		// ---------------------------------------------------------------------------------------------
+		// RGBの平均値
+		// ---------------------------------------------------------------------------------------------
+		getAverage: function(color) {
+			var average = Math.floor((color.r + color.g + color.b) / 3)
+			return average;
+		},
+		// ---------------------------------------------------------------------------------------------
+		// 元の画像を表示
+		// ---------------------------------------------------------------------------------------------
+		renderOriginalImage: function() {
+			if ( this.originData !== null ) {
+				this.context.putImageData(this.originData, 0, 0);
+			}
+		},
+		// ---------------------------------------------------------------------------------------------
+		// グレースケール化
+		// ---------------------------------------------------------------------------------------------
+		renderGrayScale: function(mode) {
+			// 新しく作る画像の準備
+			var create = this.context.createImageData(this.canvas.width, this.canvas.height);
+			// 今描画している画像のデータを取得
+			// var origin = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+			var origin = this.originData;
+			var get = null;
+			switch(mode) {
+				// 輝度
+				case 'brightness':
+					get = this.getLuminance;
+					break;
+				// 彩度	
+				case 'saturation':
+					get = this.getSaturation;
+					break;
+				// 単純平均法
+				case 'average':
+					get = this.getAverage;
+					break;
+				default:
+					get = this.getLuminance;
+					break;
+			}
+			for( var i = 0; i < origin.data.length/4; i++ ) {
+				var p = i*4;
+				// 輝度値を取得
+				var luminance = get({ r: origin.data[p], g: origin.data[p+1], b: origin.data[p+2] });
+				// 冗長だが Uint8ClampedArray に対して push() は出来ないようだ
+				create.data[p+0] = luminance;
+				create.data[p+1] = luminance;
+				create.data[p+2] = luminance;
+				create.data[p+3] = 255;
+			}
+			this.context.putImageData(create, 0, 0);
 		}
 	};
 
