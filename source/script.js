29c72164e409a015b8d7d08005534d7373e2afb1
windyakin <windyakin@gmail.com>
2015-07-11 14:22:27 +0900

    組織的ディザ法の2値化処理追加

--- source/script.js
+++ source/script.js
@@ -178,7 +178,75 @@
 			}
 			// 作成した画像に置き換え
 			this.context.putImageData(create, 0, 0);
+		},
+		// ---------------------------------------------------------------------------------------------
+		// 組織的ディザ法による2値化
+		// ---------------------------------------------------------------------------------------------
+		renderPatternDitherImage: function(mode) {
+
+			var width = this.canvas.width;
+			var height = this.canvas.height;
+
+			var create = this.context.createImageData(width, height);
+			var origin = this.context.getImageData(0, 0, width, height);
+
+			// マトリックス
+			var matrixData = {
+				// Bayer型
+				bayer: [0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7 ,13, 5],
+				// ハーフトーン型
+				harftone: [10, 4, 6, 8, 12, 0, 2, 14, 7, 9, 11, 5, 3, 15, 13 ,1],
+				// Screw型
+				screw: [13, 7, 6, 12, 8, 1, 0, 5, 9, 2, 3, 4, 14, 10, 11, 15]
+			};
+
+			var matrix = matrixData[mode];
+
+			for ( var i = 0; i < height; i+=4 ) {
+
+				// 4で割り切れない時用
+				var yrange = ( height - i < 4 ? height - i : 4 );
+
+				for ( var j = 0; j < width; j+=4 ) {
+
+					// 左上の点
+					var datum = j + i * width;
+
+					// 4で割り切れない時用
+					var xrange = ( width - j < 4 ? width - j : 4 );
+
+					// マトリックスの適用
+					for ( var y = 0; y < yrange; y++ ) {
+						for ( var x = 0; x < xrange; x++ ) {
+							// ピクセルの値
+							var p = (datum + x + y * width) * 4;
+							if ( ! (origin.data[p] === origin.data[p+1] && origin.data[p] === origin.data[p+2]) ) {
+								console.error("Please input gray scale image.");
+								return;
+							}
+							// マトリックス
+							var m = x + (y * 4);
+							if ( matrix[m]*16 < origin.data[p] ) {
+								create.data[p+0] = 255;
+								create.data[p+1] = 255;
+								create.data[p+2] = 255;
+							}
+							else {
+								create.data[p+0] = 0;
+								create.data[p+1] = 0;
+								create.data[p+2] = 0;
+							}
+							create.data[p+3] = 255;
+						}
+					}
+
+				}
+			}
+
+			// 作成した画像に置き換え
+			this.context.putImageData(create, 0, 0);
 		}
+
 	};
 
 	$(document).ready(function($) {
